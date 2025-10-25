8b8b94bd70c9b78e0707580b25be6a44457b7088
asilva <austinmsilva97@gmail.com>
2025-10-25 12:23:48 -0500

    refactor: remove unused PhoneticsPage and vocalize function for cleaner codebase

--- src/app/fantasy-name-generator/lib/markov-chain-language-models/phonetics.ts
+++
@@ -1,6 +0,0 @@
-import { LanguageDefinition } from "./core";
-
-// Stub vocalize function for now
-export function vocalize(name: string[], languageDefinition: LanguageDefinition) {
-	console.log('Vocalizing:', name);
-}
